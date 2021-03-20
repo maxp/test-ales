77aa91e50c09c3f81224913747386614f623bc1f
Maxim Penzin <maxp@ya.ru>
2021-03-20 11:49:08 +0800

    random graph generator

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 # Graph Traversal
 
+Run nrepl:
+
 ```bash
 clj -M:dev:nrepl
 ```
-
-## misc
-
-<http://hueypetersen.com/posts/2013/06/25/graph-traversal-with-clojure/>
-
-
--- src/gt/weighted.clj
+++ src/gt/weighted.clj
@@ -38,3 +38,48 @@
   (seq-graph-bfs WG :1) ; => (:1 :2 :3 :4)
 
   ,)
+
+; - - - - - - - - - - - - - - - - - - -
+
+(def ^:const WEIGHT_RANGE 10)
+
+(defn triangle-pairs [n]
+  (mapcat
+    (fn [i]
+      (map #(list i %) (range (inc i) n)))
+    (range n)))
+;
+
+(defn random-graph 
+  "generate wighted directed graph in form
+    {vertice [
+                [destination-vertice weight]
+                ...
+              ]
+              ...
+              }
+  "
+  [num-vertices num-edges]
+  (let [graph (into {} (map #(vector % []) (range num-vertices)))
+        ;; NOTE: inefficient for large number of vertices !
+        vpairs (->> (triangle-pairs num-vertices) (shuffle) (take num-edges))]
+    ;
+    (->> vpairs
+      (reduce
+        (fn [g [v-from v-to]]
+          (let [weight (rand-int WEIGHT_RANGE)]
+            (update g v-from
+              conj [v-to weight])))
+        graph))))
+
+(comment
+
+  (triangle-pairs 5)
+  ;; => ([0 1] [0 2] [0 3] [0 4] [1 2] [1 3] [1 4] [2 3] [2 4] [3 4])
+
+  (random-graph 6 10)
+  ;; => {0 [[4 7] [1 8] [5 8] [2 1] [3 3]], 1 [[4 4] [3 9] [2 9]], 2 [[4 1]], 3 [[5 0]], 4 [], 5 []}
+
+
+  (list 1 2)
+  ,)
